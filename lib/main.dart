57eff6034434a858029294987e87a1d1fc80e03d
Serkan <serkanok1983@yahoo.com>
2020-04-25 11:32:09 +0300

    quiz brain made private

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -70,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 // The user picked true.
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionAnswer(questionNumber);
                 if (correctAnswer == true) {
                   print('User got it right');
                 } else {
@@ -98,7 +98,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 // The user picked false.
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionAnswer(questionNumber);
                 if (correctAnswer == false) {
                   print('User got it right');
                 } else {
